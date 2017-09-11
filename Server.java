c5a3958a06f4e9ba7ea2d994b00ecab326b51561
gabfleury <gtfcurado3@hotmail.com>
2017-09-11 11:05:24 -0300

    'server'

---
+++ Server.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+	public static void main(String[] args) throws Exception {
+		int port = 8082;
+	    ServerSocket serverSocket = new ServerSocket(port);
+	    System.err.println("O server esta rodando na porta : " + port);
+
+	   
+	}
+
+}
+
